5681c1d22222ed042a6eec9453c01acda3998eb1
Nurbek Ismailov <nurbek82@gmail.com>
2019-11-07 16:26:32 -0800

    removed fragment

--- src/App.js
+++ src/App.js
@@ -22,11 +22,7 @@ render() {
       <div className='box backlog'>
         <div className='header card'>Backlog</div>
         {this.state.backlog.map(card=> {
-          return (
-            <React.Fragment>
-            <div className="card">{card}</div>
-            </React.Fragment>
-          )
+          return <div className="card">{card}</div>
         })}
       <div onClick={()=>this.addCard('backlog')} className="add-card">+ Add Card</div>
       </div>
@@ -35,11 +31,7 @@ render() {
       <div className='box todo'>
         <div className='header card'>ToDo</div>
         {this.state.todo.map(card=> {
-          return (
-            <React.Fragment>
-            <div className="card">{card}</div>
-            </React.Fragment>
-            )
+          return <div className="card">{card}</div>
         })}
         <div onClick={()=>this.addCard('todo')} className="add-card">+ Add Card</div>
       </div>
@@ -49,11 +41,7 @@ render() {
       <div className='box progress'>
       <div className='header card'>Progress</div>
     {this.state.progress.map(card=> {
-      return (
-        <React.Fragment>
-        <div className="card">{card}</div>
-        </React.Fragment>
-      )
+      return <div className="card">{card}</div>
     })}
         <div onClick={()=>this.addCard('progress')} className="add-card">+ Add Card</div>
       </div>
@@ -63,11 +51,7 @@ render() {
       <div className='box done'>
         <div className='header card'>Done</div>
   {this.state.done.map(card=>{
-      return (
-      <React.Fragment>
-      <div className='card'>{card}</div>
-      </React.Fragment>
-    )
+      return <div className="card">{card}</div>
   }
   )}
       <div onClick={()=>this.addCard('done')} className="add-card">+ Add Card</div>
